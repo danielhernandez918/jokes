4a9f86aff5d62faa8e90ca7ef6681f783e398916
Daniel Hernandez <danielhernandez918@gmail.com>
2022-03-18 20:16:01 -0700

    added other method for controllers

--- controllers/jokes.controller.js
+++ controllers/jokes.controller.js
@@ -2,42 +2,82 @@
 const {Jokes} = require('./../models/jokes.model')
 
 //all jokes
-module.exports.allJokes = (req,res)=>{
-    Jokes.find()
-        .then(joke => res.json(joke))
-        .catch(err=>res.json(err))
-}
 
-    //one joke
-module.exports.oneJoke = (req,res)=>{
-    const id = req.params.id
-    Jokes.findOne({_id:id})
-        .then(joke => res.json(joke))
-        .catch(err=>res.json(err))
-}
+// module.exports.allJokes = (req,res)=>{
+//     Jokes.find()
+//         .then(joke => res.json(joke))
+//         .catch(err=>res.json(err))
+// }
 
-// create
-module.exports.createJoke = (req,res)=>{
-    Jokes.create(req.body)
+//     //one joke
+// module.exports.oneJoke = (req,res)=>{
+//     const id = req.params.id
+//     Jokes.findOne({_id:id})
+//         .then(joke => res.json(joke))
+//         .catch(err=>res.json(err))
+// }
+
+// // create
+// module.exports.createJoke = (req,res)=>{
+//     Jokes.create(req.body)
+//         .then(response => res.json(response))
+//         .catch(err=>res.json(err))
+// }
+
+// // update
+// module.exports.updateJoke = (req,res)=>{
+//     const id = req.params.id
+//     Jokes.findOneAndUpdate (
+//         {_id:id}, 
+//         req.body, 
+//         {new: true, runValidators:true}
+//     )
+//     .then(response => res.json(response))
+//     .catch(err=>res.json(err))
+// }
+
+// // delete
+// module.exports.deleteJoke = (req,res)=>{
+//     Jokes.deleteOne ({_id:req.params.id})
+//         .then(response => res.json(response))
+//         .catch(err=>res.json(err))
+// }
+
+module.exports = {
+    // all jokes
+    allJokes : (req,res)=>{
+        Jokes.find()
+            .then(joke => res.json(joke))
+            .catch(err=>res.json(err))
+    },
+    // one joke
+    oneJoke : (req,res)=>{
+        const id = req.params.id
+        Jokes.findOne({_id:id})
+            .then(joke => res.json(joke))
+            .catch(err=>res.json(err))
+    }, 
+    // create 
+    createJoke : (req,res)=>{
+        Jokes.create(req.body)
+            .then(response => res.json(response))
+            .catch(err=>res.json(err))
+    },
+    // update
+    updateJoke : (req,res)=>{
+        const id = req.params.id
+        Jokes.findOneAndUpdate (
+            {_id:id}, 
+            req.body, 
+            {new: true, runValidators:true}
+        )
         .then(response => res.json(response))
         .catch(err=>res.json(err))
-}
-
-// update
-module.exports.updateJoke = (req,res)=>{
-    const id = req.params.id
-    Jokes.findOneAndUpdate (
-        {_id:id}, 
-        req.body, 
-        {new: true, runValidators:true}
-    )
-    .then(response => res.json(response))
-    .catch(err=>res.json(err))
-}
-
-// delete
-module.exports.deleteJoke = (req,res)=>{
-    Jokes.deleteOne ({_id:req.params.id})
-        .then(response => res.json(response))
-        .catch(err=>res.json(err))
+    },
+    // delete
+    deleteJoke : (req,res)=>{
+        Jokes.deleteOne ({_id:req.params.id})
+            .then(response => res.json(response))
+            .catch(err=>res.json(err))
+    }
 }
